14e4027bf981b0b41ee17e3317096f4d668b2719
j. jillian <judith.mediosdigitales@gmail.com>
2022-06-07 12:22:14 +0200

    axios is working

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { send } = require("express/lib/response");
 const Beach = require('../models/Beach.model');
+const axios = require('axios')
 
 /* GET home page */
 router.get("/", async (req, res, next) => {
@@ -16,10 +17,28 @@ router.get('/forecast', async (req, res, next) => {
   });
 
 router.get('/forecast-detail/:id', async (req, res) => {
-const beach = await Beach.findOne({spotId:req.params.id})
-console.log('beach', beach)
-res.render('forecast-detail', {beach})
+  try {
+    const {id} = req.params
+    const singleSpot = await axios.get(`https://services.surfline.com/kbyg/spots/forecasts/wave?spotId=${id}&days=1&intervalHours=24&maxHeights=false`)
+    
+  const beach = await Beach.findOne({spotId:req.params.id})
+  const humanRelation = singleSpot.data.data.wave[0].surf.humanRelation;
+  const max = singleSpot.data.data.wave[0].surf.max;
+  const min= singleSpot.data.data.wave[0].surf.min;
+  const spotDetails = {
+    humanRelation:humanRelation,
+    max : max,
+    min: min,
+    beach:beach
+}
+  // console.log('beach', max, min)
+  res.render('forecast-detail', {spotDetails})
+  } catch (error) {
+    console.log('We have an error', error)
+  }
 })
+
+
     
 
 
